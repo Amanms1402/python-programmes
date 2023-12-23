72563df558e4a3fcbe2f712ac03d7098cd3e6062
Amanms1402 <119517551+Amanms1402@users.noreply.github.com>
2023-12-23 21:02:10 +0530

    first commit

---
+++ Infinity/infinity.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+def setup():
+	bgcolor("black")
+	color("red")
+	speed(11)
+	right(45)
+
+if __name__ == '__main__':
+	setup()
+
+	for i in range(150):
+		circle(30)
+	
+		if 7 < i < 62:
+			left(5)
+		if 80 < i < 133:
+			right(5)
+		if i < 80:
+			forward(10)
+		else:
+			forward(5)
+		turtle.done()
