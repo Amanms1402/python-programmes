d71623205f1ba064e4ddffebe02fa1c7c0185386
Amanms1402 <119517551+Amanms1402@users.noreply.github.com>
2024-02-21 12:21:04 +0530

    fs

--- experiment no 1.py
+++ experiment no 1.py
@@ -1,9 +1,10 @@
 start= int(input("Enter the start value:"))
 stop=int(input("Enter the end value:"))
-step=int(input("Enter trhe difference between each Number: "))
+step=int(input("Enter the difference between each Number: "))
 
 
 numbers_list= list(range(start,stop,step))
 
 for num in numbers_list:
     print(num)
+    
